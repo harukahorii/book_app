0641e7cb59f59f69e68d1c1b09f1afc2b19a0806
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-08-24 12:50:42 +0900

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+
+use App\Book;
+use Illuminate\Http\Request;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/', ['middleware' => 'auth',function(){
+        $books = Book::all();
+        return view('books',[
+            'books' => $books
+        ]);
+    }]);
+    // 多分ここのbookはpostした後の行き先
+    Route::post('/book', ['middleware' => 'auth',function(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $book = new Book;
+        $book->title = $request->name;
+        $book->save();
+        // 保存した後の行き先
+        return redirect('/');
+    }]);
+
+    Route::delete('/book/{book}', ['middleware' => 'auth',function(Book $book){
+        $book->delete();
+
+        return redirect('/');
+    }]);
+    
+    Route::auth();
+});
