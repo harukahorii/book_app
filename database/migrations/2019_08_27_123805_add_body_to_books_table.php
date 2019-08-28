e9eb1d4b51694335df9c93a0f2c0f1df441b6187
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-08-28 10:07:33 +0900

    add column books table

--- database/migrations/2019_08_27_123805_add_body_to_books_table.php
+++ database/migrations/2019_08_27_123805_add_body_to_books_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTextToBooksTable extends Migration
+class AddBodyToBooksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,6 +14,7 @@ class AddTextToBooksTable extends Migration
     public function up()
     {
         Schema::table('books', function (Blueprint $table) {
+            $table->string('body')->default("");
             
         });
     }
